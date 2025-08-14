50cc26455e4b7e62c590e7067fbaa5ee0d436850
Alison <alisonjoyforster@gmail.com>
2025-08-14 14:43:06 -0400

    Removing WeatherForecast Boilerplate

---
+++ GeocitiesTimes.Server/Controllers/HackerNewsController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeocitiesTimes.Server.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HackerNewsController : ControllerBase
+{
+    
+}
