32d693046ed37e538f4b4d0b79e27ab07a2aedad
Alison <alisonjoyforster@gmail.com>
2025-08-17 08:38:57 -0400

    Choosing a smaller Default Max Concurrency

--- GeocitiesTimes.Server/Constants.cs
+++ GeocitiesTimes.Server/Constants.cs
@@ -2,14 +2,16 @@
 {
     public class Constants
     {
-        /* This number was chosen after some very quick experimentation to see what delivered
-         * best performance.  Please feel free to modify it.
+        /* This number was chosen to protect the Hacker News API from being swamped with requests.  
+         * You can raise it if you need better performance, but please keep in mind that the API
+         * has no rate limiter so we must be our own rate limiter.
          */
-        public const int DefaultMaxConcurrency = 25;
+        public const int DefaultMaxConcurrency = 5;
 
-        /* This number was chosen to be twice the amount of concurrent operations that will run
-         * when GetStoryListFromCacheOrClient.  The ensures that the semaphore will be fully utilized.
+        /* This number was chosen to ensure there is always a full queue ready to go for GetStoryListFromCacheOrClient
+         * to process.  The semaphore will always be full.  However, we keep our batches small enough that we will
+         * know when it is time to early exit before processing too many resources.
          */
-        public const int MaxBatchSize = DefaultMaxConcurrency * 2;
+        public const int MaxBatchSize = DefaultMaxConcurrency * 5;
     }
 }
